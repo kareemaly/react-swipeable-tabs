406f290ce7e8c0c8b3a8fb85f59af632615ed348
Kareem Mohamed <kareem3d.a@gmail.com>
2017-04-30 16:23:33 +0300

    Adding build path to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     path.resolve(__dirname, 'src/index.js')
   ],
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
